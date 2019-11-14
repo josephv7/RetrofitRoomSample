c1bfcd786276e2bd6488803b0a92291aad166b73
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-11-15 00:56:05 +0530

    :sparkles: created view model

---
+++ app/src/main/java/com/iamjosephvarghese/retrofitroomsample/viewModel/ViewModel.java
@@ -0,0 +1,33 @@
+package com.iamjosephvarghese.retrofitroomsample.viewModel;
+
+import android.app.Application;
+
+import com.iamjosephvarghese.retrofitroomsample.model.Quote;
+import com.iamjosephvarghese.retrofitroomsample.repository.QuoteRepository;
+
+import java.util.List;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class ViewModel extends AndroidViewModel {
+
+    private QuoteRepository quoteRepository;
+
+    public ViewModel(Application application){
+        super(application);
+        quoteRepository = new QuoteRepository(application);
+
+
+    }
+
+    public LiveData<List<Quote>> getSchedule(String author){
+        return quoteRepository.getQuotes(author);
+    }
+
+
+    public void insert(Quote quote){
+        quoteRepository.insert(quote);
+    }
+
+}
